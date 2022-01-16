d7310e1ed0a3eb6a1e6b9736badc0371b8402e73
Vince <sikatunar@gmail.com>
2022-01-16 18:26:45 +0800

    TeamSection

---
+++ src/components/TeamSection/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function TeamSection() {
+    return (
+        <TeamContainer>
+            <TeamBg>
+                
+              
+            </TeamBg>
+
+            <TeamContent>
+                <TeamRow>
+                        <Column1>
+                            <TeamImgWrapper>
+                               
+                            </TeamImgWrapper>
+                        </Column1>
+
+                        <TeamRow2>
+                            <Column2>
+                                <Column2Wrapper>
+                                    
+                                </Column2Wrapper>
+                            </Column2> 
+                        </TeamRow2>
+                                    
+                </TeamRow>
+            </TeamContent>
+        </TeamContainer>
+    )
+}
+
+export default TeamSection
